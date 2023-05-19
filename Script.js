cdaef6d508be99938e106bfe2a5e57a82969ec9a
Aaron <aaronalvarado200@yahoo.com>
2023-05-19 19:34:25 +0200

    new Code

--- Script.js
+++ Script.js
@@ -1,9 +1,7 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("links");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+const mobile_menue = document.querySelector(".mobile-nav");
+const menu_btn = document.querySelector('.hamburger');
+
+menu_btn.addEventListener('click', function() {
+menu_btn.classList.toggle('is-active');
+mobile_menue.classList.toggle("is-active")
+});
